00bafb307709a5e9db139a2c1343e896944f6d42
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2020-08-25 12:13:02 -0500

    Create joins-solution.sql

---
+++ joins-solution.sql
@@ -0,0 +1,37 @@
+-- TASKS
+-- Get all customers and their addresses.
+  SELECT "first_name", "last_name", "addresses".* FROM "customers" 
+  JOIN "addresses" ON "customers".id = "addresses".customer_id;
+  
+-- Get all orders and their line items (orders, quantity and product).
+  SELECT *, "line_items".*, "orders".* FROM "products"
+  JOIN "line_items" ON "orders".id = "line_items".product_id
+  JOIN "warehouse_product" ON "products".id = "warehouse_product".product_id;
+
+-- Which warehouses have cheetos?
+  SELECT *, "products".description FROM "warehouse"
+  INNER JOIN "warehouse_product" ON "warehouse".id = "warehouse_product".warehouse_id
+  INNER JOIN "products" ON "warehouse_product".product_id = "products".id
+  WHERE "products".description = 'cheetos';
+
+-- Which warehouses have diet pepsi?
+
+
+-- Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+
+
+-- How many customers do we have?
+
+
+-- How many products do we carry?
+
+
+-- What is the total available on-hand quantity of diet pepsi?
+
+
+-- STRETCH
+-- How much was the total cost for each order?
+
+-- How much has each customer spent in total?
+
+-- How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
